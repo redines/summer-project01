8eaa1d3c652d535de2f2c14cba08b276569610f9
Pontus Göth <pontus.g91@gmail.com>
2018-07-07 21:04:02 +0200

    changed name of tables in table users to more sensible ones

--- count.sql
+++ count.sql
@@ -3,8 +3,8 @@ CREATE DATABASE count;
 
 create table users (
     id INT NOT NULL AUTO_INCREMENT, 
-    name VARCHAR(20) NOT NULL, 
+    user_name VARCHAR(20) NOT NULL, 
     word VARCHAR(10) NOT NULL, 
-    count INT NOT NULL, PRIMARY KEY(id)
+    count_word INT NOT NULL, PRIMARY KEY(id)
 ) ENGINE InnoDB;
 
